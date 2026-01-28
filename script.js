4e1a0ba23828d38910fc37a117dc7c4433b36061
bhumi576 <bbhumi576@gmail.com>
2026-01-28 19:47:11 +0530

    Enhanced portfolio design and added project

---
+++ script.js
@@ -0,0 +1,29 @@
+// function scrollToSection(id) {
+//   document.getElementById(id).scrollIntoView({
+//     behavior: "smooth"
+//   });
+// }
+
+// function showMessage(e) {
+//   e.preventDefault();
+//   document.getElementById("msg").innerText =
+//     "Thank you! Your message has been sent 😊";
+// }
+
+
+
+
+
+
+
+function scrollToSection(id) {
+  document.getElementById(id).scrollIntoView({
+    behavior: "smooth"
+  });
+}
+
+function showMessage(e) {
+  e.preventDefault();
+  document.getElementById("msg").innerText =
+    "Thank you! Your message has been sent 😊";
+}
